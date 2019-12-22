e4e43ed634c907aec2b2781b38f87e51099d43e3
KateRaymundi <kateacristina@outlook.com>
2019-12-22 02:07:49 +0000

    add some tinkering code

--- tinkering.js
+++ tinkering.js
@@ -1,6 +1,15 @@
+/*
 function multiply (a, b) {
   return a * b 
 }
 const result = multiply (2, 4)
 
 console.log(result)
+*/
+
+
+for (var x = 0; x < 5; x = x + 1) {
+  for (var y = 0; y < 3; y = y + 1) {
+    console.log(x + "," + y);
+  }
+}
