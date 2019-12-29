6c251022d179bb3114ca66a49b8514f8a328d532
KateRaymundi <kateacristina@outlook.com>
2019-12-29 22:02:49 +0000

    tinkering adjustment

--- tinkering.js
+++ tinkering.js
@@ -6,10 +6,26 @@ const result = multiply (2, 4)
 
 console.log(result)
 */
-
-
-for (var x = 0; x < 5; x = x + 1) {
-  for (var y = 0; y < 3; y = y + 1) {
-    console.log(x + "," + y);
-  }
+/*
+function addTwo(x,y) {
+  return x + y + 2;
 }
+
+var result1 = addTwo(5,10);
+var result2 = addTwo(15,210);
+var result3 = addTwo(315,310);
+
+//console.log(result);
+
+console.log(result1 + '-' + result2 + '-' + result3); */
+
+var x = 1;
+
+function addTwo() {
+  x = x + 2;
+}
+
+addTwo();
+addTwo();
+x = x + 1;
+console.log(x);
